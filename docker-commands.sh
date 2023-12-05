1c43992ad8eb42c45c1723a37df2ff4cab46c3f7
Bismarck Villca <bismarck.villca@gmail.com>
2023-12-05 16:49:20 -0400

    docker commands

---
+++ docker-commands.sh
@@ -0,0 +1,17 @@
+
+
+
+# Build maven project
+mvn clean -U package -Dmaven.test.skip
+
+# Build
+docker build -t registry.gitlab.com/villcabo/polyglot-work/apppay .
+docker build -t registry.gitlab.com/villcabo/polyglot-work/appsecurity .
+docker build -t registry.gitlab.com/villcabo/polyglot-work/appinvoice .
+docker build -t registry.gitlab.com/villcabo/polyglot-work/apptransaction .
+
+# Push
+docker push registry.gitlab.com/villcabo/polyglot-work/apppay
+docker push registry.gitlab.com/villcabo/polyglot-work/appsecurity
+docker push registry.gitlab.com/villcabo/polyglot-work/appinvoice
+docker push registry.gitlab.com/villcabo/polyglot-work/apptransaction
